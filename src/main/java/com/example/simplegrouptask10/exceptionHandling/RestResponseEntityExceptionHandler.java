b471d065ee68b6b6cabedaa4b61c05831b572b9f
Slipeer <slipeer1@list.ru>
2022-07-21 16:02:53 +0500

    refactor1

---
+++ src/main/java/com/example/simplegrouptask10/exceptionHandling/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.simplegrouptask10.exceptionHandling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends
+        ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ProductIncorrectData> handleException(Exception exception) {
+        ProductIncorrectData productIncorrectData =  new ProductIncorrectData(exception.getMessage());
+        return new ResponseEntity<>(productIncorrectData, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ProductIncorrectData> handleException(EntityNotFoundException exception) {
+        ProductIncorrectData productIncorrectData =  new ProductIncorrectData(exception.getMessage());
+        return new ResponseEntity<>(productIncorrectData, HttpStatus.NOT_FOUND);
+    }
+}
